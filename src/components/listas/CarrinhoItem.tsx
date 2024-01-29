5a0c896a152ce42354d63497ad33733ad29c2b72
erickandrade18 <ericksilvandrade176@gmail.com>
2024-01-29 13:47:46 -0300

    no message

--- src/components/listas/CarrinhoItem.tsx
+++ src/components/listas/CarrinhoItem.tsx
@@ -6,8 +6,8 @@ export default function CarrinhoItem(props: ItemCarrinho) {
         <div className={`
             flex rounded-full bg-blue-500 
         `}>
+            <span className="w-7 h-7 rounded-full p-2 bg-blue-700">{props.quantidade}</span>
             {props.produto.nome}
-            {props.quantidade}
         </div>
     )
 }
